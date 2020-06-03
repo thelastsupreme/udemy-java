9796fd45f1e7d26788dbf9f595e08d8e9d6abcee
thelastsupreme <39990949+thelastsupreme@users.noreply.github.com>
2020-06-03 23:47:15 +0530

    test

--- HelloWorld/src/Hello.java
+++
@@ -1,7 +0,0 @@
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("hello");
-        int FirstNumber=5;
-        System.out.println(FirstNumber);
-    }
-}
