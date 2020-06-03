cd5ca524edd1712f2c0e2afa9737442397516b6b
thelastsupreme <39990949+thelastsupreme@users.noreply.github.com>
2020-06-03 23:40:06 +0530

    ..
    ..

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("hello");
+        int FirstNumber=5;
+        System.out.println(FirstNumber);
     }
 }
